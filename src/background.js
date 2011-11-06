c88f4a737a2670a7207a000f779fa1b40aaa5a53
viclm <fliuming@gmail.com>
2011-11-06 21:49:30 +0800

    ...

--- src/background.js
+++ src/background.js
@@ -1,41 +1,50 @@
-(function (window, document, undefined) {
+
     var audio = document.querySelector('audio');
     var helper = document.querySelector('#helper');
     var isPlay = false;
-	var isRepeat = false;
+    var isRepeat = false;
     var playList = [];
     var h = [];
     var current = 0;
     var time = 0;
-	var canplaythrough = false;
+    var canplaythrough = false;
     var p = null;
 
+    oauth(function () {});
 
-    helper.addEventListener('canplaythrough', function () {canplaythrough = true;}, false);
 
-    audio.addEventListener('canplaythrough', function () {console.log('canplaythrough')
-		p && p.postMessage({cmd: 'canplaythrough', status: true});
-		bufferNext();
-	},false);
+    helper.addEventListener('canplaythrough', function () {canplaythrough = true;console.log('helper canplaythrough')}, false);
 
-	audio.addEventListener('timeupdate', function () {
+    audio.addEventListener('loadstart', function () {
+        canplaythrough = false;
+        p && p.postMessage({cmd: 'canplaythrough', status: false});
+    },false);
+
+    audio.addEventListener('canplaythrough', function () {
+        canplaythrough = true;
+        p && p.postMessage({cmd: 'canplaythrough', status: true});
+        bufferNext();
+        console.log(playList[current])
+    },false);
+
+    audio.addEventListener('timeupdate', function () {
         var currentTime = Math.floor(audio.currentTime);
-		if (currentTime > time) {
-			time = currentTime;
-			if(p) {p.postMessage({cmd: 'progress', time: time, length: Math.floor(audio.duration)});}
-		}
+        if (currentTime > time) {
+            time = currentTime;
+            if(p) {p.postMessage({cmd: 'progress', time: time, length: Math.floor(audio.duration)});}
+        }
     },false);
 
     audio.addEventListener('ended', function () {
-		if (!isRepeat) {
-			if (h.length && h[h.length - 1].indexOf(playList[current].sid) > -1) {h.pop();}
-			h.push('|' + playList[current].sid + ':p');
-			current += 1;
-			audio.src = playList[current].url;
-		}
+        if (!isRepeat) {
+            //if (h.length && h[h.length - 1].indexOf(playList[current].sid) > -1) {h.pop();}
+            h.push('|' + playList[current].sid + ':p');
+            current += 1;
+            audio.src = playList[current].url;
+        }
         time = 0;
         audio.play();
-		if (p) {p.postMessage({cmd: 'set', song: getCurrentSongInfo()});}
+        if (p) {p.postMessage(getCurrentSongInfo());}
     }, false);
 
     chrome.extension.onConnect.addListener(function(port) {
@@ -54,82 +63,89 @@
                         audio.pause();
                     }
                     break;
-				case 'next':
-					if (playList[current]) {
-						if (h.length && h[h.length - 1].indexOf(playList[current].sid) > -1) {h.pop();}
-						h.push('|' + playList[current].sid + ':s');
-					
-						current += 1;
-						if (playList[current]) {
-							audio.src = playList[current].url;
-							audio.play();
-							time = 0;
-							port.postMessage({cmd: 'set', song: getCurrentSongInfo()});
-						}
-						else {
-							fetchSongs(function () {
-								audio.src = playList[current].url;
-								audio.play();
-								time = 0;
-								port.postMessage({cmd: 'set', song: getCurrentSongInfo()});
-							});
-						}
-					}
-					break;
-				case 'prev':
-					if (current) {
-						current -= 1;
-						audio.src = playList[current].url;
-						audio.play();
-						time = 0;
-					}
-					port.postMessage({cmd: 'set', song: getCurrentSongInfo()});
-					break;
-				case 'volume':
-					audio.volume = msg.value / 100;
-				case 'repeat':
-					isRepeat = msg.status;
-					break;
-				case 'love':
-					if (status) {
-						h.push('|' + playList[current].sid + ':r');
-					}
-					else {
-						h.pop();
-					}
-					break;
-				case 'trash':
-					if (playList[current]) {
-						if (h.length && h[h.length - 1].indexOf(playList[current].sid) > -1) {h.pop();}
-						h.push('|' + playList[current].sid + ':b');
-					
-						current += 1;
-						if (playList[current]) {
-							audio.src = playList[current].url;
-							audio.play();
-							time = 0;
-							port.postMessage({cmd: 'set', song: getCurrentSongInfo()});
-						}
-						else {
-							fetchSongs(function () {
-								audio.src = playList[current].url;
-								audio.play();
-								time = 0;
-								port.postMessage({cmd: 'set', song: getCurrentSongInfo()});
-							});
-						}
-					}
-					break;
+                case 'next':
+                    if (playList[current]) {
+                        h.push('|' + playList[current].sid + ':s');
+
+                        if (playList[current + 1]) {
+                            current += 1;
+                            audio.src = playList[current].url;
+                            audio.play();
+                            time = 0;
+                            port.postMessage(getCurrentSongInfo());
+                        }
+                        else {
+                            fetchSongs(function () {
+                                current += 1;
+                                audio.src = playList[current].url;
+                                audio.play();
+                                time = 0;
+                                port.postMessage(getCurrentSongInfo());
+                            });
+                        }
+                    }
+                    break;
+                case 'prev':
+                        if (current) {
+                        current -= 1;
+                        audio.src = playList[current].url;
+                        audio.play();
+                        time = 0;
+                    }
+                    port.postMessage(getCurrentSongInfo());
+                    break;
+                case 'volume':
+                    audio.volume = msg.value / 100;
+                    break;
+                case 'repeat':
+                    isRepeat = msg.status;
+                    break;
+                case 'love':
+                    if (msg.status) {
+                        h.push('|' + playList[current].sid + ':r');
+                        playList[current].like = '1';
+                    }
+                    else {
+                        h.push('|' + playList[current].sid + ':u');
+                        playList[current].like = '0';
+                    }
+                    fetchSongs(function (){});
+                    h.pop();
+                    break;
+                case 'trash':
+                    h.push('|' + playList[current].sid + ':b');
+                    fetchSongs(function (){});
+                    h.pop();
+                    playList.splice(current, 1);
+
+                    //current += 1;
+                    if (playList[current]) {
+                        audio.src = playList[current].url;
+                        audio.play();
+                        time = 0;
+                        port.postMessage(getCurrentSongInfo());
+                    }
+                    else {
+                        current -= 1;
+                        fetchSongs(function () {
+                            current += 1;
+                            audio.src = playList[current].url;
+                            audio.play();
+                            time = 0;
+                            port.postMessage(getCurrentSongInfo());
+                        });
+                    }
+                    break;
                 case 'get':
                     if (playList.length) {
-                        port.postMessage({cmd: 'set', song: getCurrentSongInfo()});
+                        port.postMessage(getCurrentSongInfo());
                     }
                     else {
                         fetchSongs(function () {
-							audio.src = playList[0].url;
-							if (isPlay) {audio.play();}
-							port.postMessage({cmd: 'set', song: getCurrentSongInfo()});
-						});
+                            audio.src = playList[0].url;
+                            if (isPlay) {audio.play();}
+                            port.postMessage(getCurrentSongInfo());
+                        });
                     }
                     break;
                 }
@@ -144,53 +160,59 @@
     });
 
     function getCurrentSongInfo() {
-        var song = playList[current];
-        song.progress = time;
-        song.length = Math.floor(audio.duration);
-		song.isPlay = isPlay;
-        return song;
+        var song = playList[current], info = {cmd: 'set'};
+        info.title = song.title;
+        info.artist = song.artist;
+        info.albumtitle = song.albumtitle;
+        info.picture = song.picture;
+        info.like = song.like;
+        info.time = time;
+        info.length = Math.floor(audio.duration);
+        info.isPlay = isPlay;
+        info.isRepeat = isRepeat;
+        info.volume = audio.volume;
+        info.canplaythrough = canplaythrough;
+        return info;
     }
 
-	function fetchSongs(fn) {
-		h = h.slice(-20);
-		ajax(
-			'get',
-			'http://douban.fm/j/mine/playlist',
-			h.length ? 'type=s&sid='+ h[h.length - 1].slice(1, -2) +'&h='+ h.join('') +'&channel=0&from=mainsite&r='+rand() : 'type=n&h=&channel=0&from=mainsite&r='+rand(),
-			10000,
-			function (client) {
-				client = JSON.parse(client.responseText);
-				for (var i = 0, len = client.song.length ; i < len ; i += 1) {
-					if (/^\d+$/.test(client.song[i].sid)) {
-						client.song[i].picture = client.song[i].picture.replace('mpic', 'lpic');
-						playList.push(client.song[i]);
-					}
-				}
-				fn();
-			}
-		);
-	}
+    function fetchSongs(fn) {
+        h = h.slice(-20);
+        ajax(
+            'get',
+            'http://douban.fm/j/mine/playlist',
+            h.length ? 'type=s&sid='+ h[h.length - 1].slice(1, -2) +'&h='+ h.join('') +'&channel=0&from=mainsite&r='+rand() : 'type=n&h=&channel=0&from=mainsite&r='+rand(),
+            10000,
+            function (client) {
+                client = JSON.parse(client.responseText);
+                for (var i = 0, len = client.song.length ; i < len ; i += 1) {
+                    if (/^\d+$/.test(client.song[i].sid)) {
+                        client.song[i].picture = client.song[i].picture.replace('mpic', 'lpic');
+                        playList.push(client.song[i]);
+                    }
+                }
+                fn();
+            }
+        );
+    }
 
-	function buffer() {
-		canplaythrough = false;
-		setTimeout(function () {
-			if (!canplaythrough) {
-				playList = playList.splice(current + 1);
-				bufferNext();
-			}
-		}, 30000);
-	}
+    function buffer() {
+        canplaythrough = false;
+        var i = current;
+        setTimeout(function () {
+            console.log(helper.buffered)
+        }, 30000);
+    }
 
-	function bufferNext() {
-        if (current === playList.length - 1) {
-            fetchSongs(function () {
-				helper.src = playList[1].url;
-				buffer();
-			});
+    function bufferNext() {
+        if (playList[current+1]) {
+            //buffer();
+            helper.src = playList[current + 1].url;
         }
         else {
-            helper.src = playList[current + 1].url;
-			buffer();
+            fetchSongs(function () {
+                //buffer();
+                helper.src = playList[1].url;
+            });
         }
     }
 
@@ -208,7 +230,7 @@
             if (!isTimeout && ((client.status >= 200 && client.status < 300) || client.status == 304)) {
                 success(client);
             }
-            else {
+            else {console.log(client)
                 error(client);
             }
         };
@@ -226,5 +248,10 @@
         }
         return str;
     }
-})(this, this.document);
+
+
+    fetchSongs(function () {
+        audio.src = playList[0].url;
+    });
+
 
--- src/popup.js
+++ src/popup.js
@@ -1,42 +1,50 @@
 (function (window, document, undefined) {
     var isPlay = false;
-	var timer = null;
+    var timer = null;
     var port = chrome.extension.connect({name: 'fm'});
     var title = document.querySelector('h1');
     var artist = document.querySelector('p');
     var progress = document.querySelector('header div');
-	var soundCtr = document.querySelector('input[type=range]')
+    var soundCtr = document.querySelector('input[type=range]')
 
     port.postMessage({cmd: 'get'});
 
     port.onMessage.addListener(function (msg) {
         switch (msg.cmd) {
-		case 'progress':
-			progress.style.width = msg.time / msg.length * 275 + 'px';
+        case 'progress':
+            progress.style.width = msg.time / msg.length * 275 + 'px';
             progress.title = strftime(msg.time) + '/' + strftime(msg.length);
-			break;
+            break;
         case 'set':
-            title.innerHTML = msg.song.title;
-            artist.innerHTML = msg.song.artist + ' | ' + msg.song.albumtitle;
-            progress.title = strftime(msg.song.time) + '/' + strftime(msg.song.length);
-            progress.style.width = msg.song.progress / msg.song.length * 275 + 'px';
-            document.body.style.backgroundImage = 'url('+ msg.song.picture +')';
-			isPlay = msg.song.isPlay;
-			if (isPlay) {
-				play.style.backgroundImage = 'url(../assets/pause.png)';
-			}
-			else {
-				play.style.backgroundImage = 'url(../assets/play.png)';
-			}
+            title.innerHTML = msg.title;
+            artist.innerHTML = msg.artist + ' | ' + msg.albumtitle;
+            progress.title = strftime(msg.time) + '/' + strftime(msg.length);
+            progress.style.width = msg.time / msg.length * 275 + 'px';
+            document.body.style.backgroundImage = 'url('+ msg.picture +')';
+            soundCtr.value = msg.volume * 100;
+            if (msg.like === '1') {love.className = 'on';}
+            if (msg.isRepeat) {repeat.className = 'on';}
+            isPlay = msg.isPlay;
+            if (isPlay) {
+                play.style.backgroundImage = 'url(../assets/pause.png)';
+            }
+            else {
+                play.style.backgroundImage = 'url(../assets/play.png)';
+            }
 
-			if (msg.song.progress === 0) {
-				loading.style.display = 'block';
-			}
+            if (!msg.canplaythrough){
+                loading.style.display = 'block';
+            }
             break;
         case 'canplaythrough':
-            loading.style.display = 'none';
-			progress.style.width = 0;
-			break;
+            if (msg.status) {
+                loading.style.display = 'none';
+            }
+            else {
+                loading.style.display = 'block';
+                progress.style.width = 0;
+            }
+            break;
         }
     });
 
@@ -52,61 +60,57 @@
         e.preventDefault();
     }, false);
 
-	next.addEventListener('click', function (e) {
-		port.postMessage({cmd: 'next'});
+    next.addEventListener('click', function (e) {
+        port.postMessage({cmd: 'next'});
         e.preventDefault();
     }, false);
 
-	prev.addEventListener('click', function (e) {
+    prev.addEventListener('click', function (e) {
         port.postMessage({cmd: 'prev'});
         e.preventDefault();
     }, false);
 
-	sound.addEventListener('click', function (e) {
-		if (soundCtr.style.display === 'none') {
-			soundCtr.style.display = 'block';
-		}
-		else {
-			soundCtr.style.display = 'none';
-		}
-		e.preventDefault();
-	}, false);
-
-	soundCtr.addEventListener('change', function (e) {
-		port.postMessage({cmd: 'volume', value: e.target.value});
-	}, false);
-
-	repeat.addEventListener('click', function (e) {
-		if (this.className === 'on') {
-			this.style.backgroundImage = 'url(../assets/repeat.png)';
-			this.className = '';
-			port.postMessage({cmd: 'repeat', status: false});
-		}
-		else {
-			this.style.backgroundImage = 'url(../assets/repeati.png)';
-			this.className = 'on';
-			port.postMessage({cmd: 'repeat', status: true});
-		}
+    sound.addEventListener('click', function (e) {
+        if (soundCtr.style.display !== 'block') {
+            soundCtr.style.display = 'block';
+        }
+        else {
+            soundCtr.style.display = 'none';
+        }
         e.preventDefault();
     }, false);
 
-	love.addEventListener('click', function (e) {
-		if (this.className === 'on') {
-			this.style.backgroundImage = 'url(../assets/love.png)';
-			this.className = '';
-			port.postMessage({cmd: 'love', status: false});
-		}
-		else {
-			this.style.backgroundImage = 'url(../assets/lovei.png)';
-			this.className = 'on';
-			port.postMessage({cmd: 'love', status: true});
-		}
+    soundCtr.addEventListener('change', function (e) {
+        port.postMessage({cmd: 'volume', value: e.target.value});
+    }, false);
+
+    repeat.addEventListener('click', function (e) {
+        if (this.className === 'on') {
+            this.className = '';
+            port.postMessage({cmd: 'repeat', status: false});
+        }
+        else {
+            this.className = 'on';
+            port.postMessage({cmd: 'repeat', status: true});
+        }
         e.preventDefault();
     }, false);
 
-	trash.addEventListener('click', function (e) {
-		port.postMessage({cmd: 'trash'});
-	}, false);
+    love.addEventListener('click', function (e) {
+        if (this.className === 'on') {
+            this.className = '';
+            port.postMessage({cmd: 'love', status: false});
+        }
+        else {
+            this.className = 'on';
+            port.postMessage({cmd: 'love', status: true});
+        }
+        e.preventDefault();
+    }, false);
+
+    trash.addEventListener('click', function (e) {
+        port.postMessage({cmd: 'trash'});
+    }, false);
 
     function strftime(seconds) {
         var minutes = Math.floor(seconds / 60), seconds = seconds % 60, str;
